7525e64c41ff90741545597205cc41383ee55090
Benoit Niveau <be.niveau@hotmail.fr>
2018-10-16 11:05:19 +0200

    quest_react_router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Home from "./Home";
+import History from "./History";
+
+import { Route, BrowserRouter, Switch, NavLink } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <BrowserRouter>
+        <div>
+          <NavLink exact to="/">
+            Home
+          </NavLink>
+          <NavLink to="/notre-histoire" activeClassName="Red">
+            History
+          </NavLink>
+
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/notre-histoire" component={History} />
+          </Switch>
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/History.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class History extends Component {
+  render() {
+    return <h2>Notre histoire</h2>;
+  }
+}
+
+export default History;
